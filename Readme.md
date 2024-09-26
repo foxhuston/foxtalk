f4cb018149bfec08393cd5f6f3eac8f3925be205
lexi Huston <hi@lexi.sh>
2024-09-26 19:08:29 -0400

    Add readme and some necessary changes to get it building on a new machine

--- Readme.md
+++ Readme.md
@@ -1,3 +1,12 @@
+# First time setup
+
+- Clone repo
+- `mkdir build`
+- Install dependencies found in `PKGBUILD`
+  - At time of writing, `sudo pacman -S stb glm glfw vulkan-headers vulkan-extra-layers vulkan-tools argparse podofo`
+- `cd build`
+- `cmake -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_BUILD_TYPE=Debug ..`
+
 # Building
 
     $ cd build
--- src/main.cpp
+++ src/main.cpp
@@ -3,28 +3,28 @@
 #include "boot/VkDisplay.hpp"
 #include "foxtalk.hpp"
 
-int main() {
-  /* Glfw container {}; */
-  VkDisplay container {};
+int main()
+{
+  Glfw container{};
+  // VkDisplay container {};
 
   Core core(&container);
 
   const auto fbSize = container.getFramebufferSize();
-  Foxtalk foxtalk {
-    core.physicalDevice(),
-    core.device(),
-    core.renderPass(),
-    static_cast<float>(fbSize.width),
-    static_cast<float>(fbSize.height),
-    MAX_FRAMES_IN_FLIGHT
-  };
+  Foxtalk foxtalk{
+      core.physicalDevice(),
+      core.device(),
+      core.renderPass(),
+      static_cast<float>(fbSize.width),
+      static_cast<float>(fbSize.height),
+      MAX_FRAMES_IN_FLIGHT};
 
-  container.mainLoop([&core, &foxtalk]() {
+  container.mainLoop([&core, &foxtalk]()
+                     {
     core.withRenderPass([&foxtalk](const vk::CommandBuffer &cmdBuffer, auto renderPassBeginInfo, auto swapchainExtent, auto imageIndex) {
         foxtalk.render(cmdBuffer, renderPassBeginInfo, swapchainExtent, imageIndex);
     });
-    core.incrementFrame();
-  });
+    core.incrementFrame(); });
 
   core.device().waitIdle();
 
