1d0c9679a776cfcb2de5651739c1785922ccb819
Fox Huston <448633+foxhuston@users.noreply.github.com>
2024-10-07 09:15:03 -0400

    CLEAN UP: deleted all of the old test code

--- rust_tests/foxtalk/build.rs
+++
@@ -1,5 +0,0 @@
-fn main() {
-    // This is needed so that dynamically linked libraries can find
-    // `pub extern "C" fn`s from the main rust code.
-    println!("cargo::rustc-link-arg=-export-dynamic");
-}
