86465d56d88caeb403ec2036c67e48cc8561341c
Fox Huston <448633+foxhuston@users.noreply.github.com>
2024-10-13 09:34:16 -0400

    added next bdd test

--- foxtalk_reactor_clib/handler_tests.cpp
+++ foxtalk_reactor_clib/handler_tests.cpp
@@ -29,7 +29,7 @@ FOXTALK_HANDLE {
 FOXTALK_FREE_TUPLE { }
 FOXTALK_TEARDOWN { }
 
-TEST_F(HandlerTests, WhoIsCool) {
+TEST_F(HandlerTests, HandlerTupleInsertTest) {
     kuzu::main::SystemConfig db_config { };
     auto db = std::make_shared<kuzu::main::Database>(":memory:", db_config);
 
@@ -68,4 +68,80 @@ TEST_F(HandlerTests, WhoIsCool) {
     ASSERT_EQ(result->getValue(0)->strVal, "Symbol");
     ASSERT_EQ(result->getValue(1)->strVal, "lexi");
 
+}
+
+TEST_F(HandlerTests, HandlerTupleRemovalTest) {
+    kuzu::main::SystemConfig db_config { };
+    auto db = std::make_shared<kuzu::main::Database>(":memory:", db_config);
+
+    Reactor r { db };
+
+    // Create Handler
+    r.claim({ {"test"s}, {"is a"s}, {"handler"s} });
+
+    r.claim({ {"test"s}, {"has init"s}, { reinterpret_cast<void *>(init) }});
+    r.claim({ {"test"s}, {"has handle"s}, { reinterpret_cast<void *>(handle) }});
+    r.claim({ {"test"s}, {"has free tuple"s}, { reinterpret_cast<void *>(free_tuple) }});
+    r.claim({ {"test"s}, {"has teardown"s}, { reinterpret_cast<void *>(teardown) }});
+    r.claim({ {"test"s}, {"has ipc_buffer"s}, { static_cast<void *>(_foxtalk_ipc_triple_buffer) }});
+
+    // Add <lexi, is a, husky>
+    r.claim({ {"lexi"s}, {"is a"s}, {"husky"s} });
+
+    // Tick
+    r.tick(); // initialize
+    r.tick(); // handle
+
+    // Assert <lexi, is, cool>
+    // d.query(...)
+    auto query = R"(MATCH(a:Noun)-[b:Predicate{string_data: "is"}]->(c:Noun{string_data:"cool"}) RETURN a.type, a.string_data;)";
+    kuzu::main::Connection conn (db.get());
+
+    auto q = conn.query(query);
+    auto results = q.get();
+    if(!results->isSuccess()) {
+        std::cerr << "Query failed with: " << results->getErrorMessage() << std::endl;
+        FAIL();
+    }
+
+    ASSERT_EQ(results->getNumTuples(), 1);
+    auto result = results->getNext();
+    ASSERT_EQ(result->getValue(0)->strVal, "Symbol");
+    ASSERT_EQ(result->getValue(1)->strVal, "lexi");
+
+    ///// THE REMOVAL ZONE /////
+
+    r.remove({{ "lexi"s }, { "is a"s }, { "husky"s }});
+    r.tick();
+    r.tick();
+
+    // <lexi, is a, husky> has been removed
+    {
+        auto query = R"(MATCH(a:Noun{string_data: "lexi"})-[b:Predicate{string_data: "is a"}]->(c:Noun{string_data:"husky"}) RETURN a.type, a.string_data;)";
+        kuzu::main::Connection conn (db.get());
+
+        auto q = conn.query(query);
+        auto results = q.get();
+        if(!results->isSuccess()) {
+            std::cerr << "Query failed with: " << results->getErrorMessage() << std::endl;
+            FAIL();
+        }
+        
+        ASSERT_EQ(results->getNumTuples(), 0);
+    }
+
+    // <lexi, is, cool> has been removed
+    {
+        auto query = R"(MATCH(a:Noun{string_data: "lexi"})-[b:Predicate{string_data: "is"}]->(c:Noun{string_data:"cool"}) RETURN a.type, a.string_data;)";
+        kuzu::main::Connection conn (db.get());
+
+        auto q = conn.query(query);
+        auto results = q.get();
+        if(!results->isSuccess()) {
+            std::cerr << "Query failed with: " << results->getErrorMessage() << std::endl;
+            FAIL();
+        }
+        ASSERT_EQ(results->getNumTuples(), 0);
+
+    }
 }
