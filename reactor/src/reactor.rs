c6e004df1d5f735c0b596bfcf5fadd4e051824e2
lexi Huston <hi@lexi.sh>
2024-10-21 09:16:34 -0400

    More rust playing around

--- reactor/src/reactor.rs
+++ reactor/src/reactor.rs
@@ -1,29 +1,32 @@
+use std::collections::HashMap;
 use std::fmt::Debug;
 use std::hash::Hash;
 use crate::types::{ReactorHandler};
 pub struct Reactor<O>
     where O: Eq + Hash + Clone + Sized + Debug{ 
-    pub handlers: Vec<ReactorHandler<O>>
+    pub handlers: Vec<ReactorHandler<O>>,
+    pub ref_counts: HashMap<O, u64>
 }
 
 impl<O> Reactor<O>
 where O: Eq + Hash + Clone + Sized + Debug  {
     pub fn new() -> Self {
         Reactor {
-            handlers: Vec::new()
+            handlers: Vec::new(),
+            ref_counts: HashMap::new()
         }
     }
 
-    // pub fn add_handler(&mut self, handler: ReactorHandler<O, Q, H>) {
-    //     self.handlers.push(handler);
-    // }
+    pub fn add_handler(&mut self, handler: ReactorHandler<O>) {
+        self.handlers.push(handler);
+    }
 
-    pub fn add_input(&mut self, input: O) {
+    pub fn insert(&mut self, input: O) {
         println!("adding o: {:?}", input);
         println!("Todo: Actually add")
     }
 
-    pub fn remove_input(&mut self, input: O) {
+    pub fn remove(&mut self, input: O) {
         println!("adding o: {:?}", input);
         println!("Todo: Actually add")
     }
@@ -42,64 +45,45 @@ mod tests {
     use super::*;
 
     #[test]
-    pub fn paper_example() {
-
-
-        struct PaperQueryHandler {
-            pub handle_calls:  Vec<u64>
-        }
-        impl PaperQueryHandler {
-            pub fn new() -> Self {
-                PaperQueryHandler {
-                    handle_calls: Vec::new()
+    pub fn paper_agg_example() {
+        
+    }
+    #[test]
+    pub fn paper_non_agg_example() {
+        fn handle(o: &u64) -> HashSet<u64> {
+            match o {
+                1 => {
+                    let mut results = HashSet::new();
+                    results.insert(2);
+                    results.insert(3);
+                    results
                 }
-            }
-        }
-
-        struct PaperQuery;
-
-        impl Queryable<u64> for PaperQuery {
-            fn matches(self, other: u64) -> bool {
-                other == 1 || other == 2
-            }
-        }
-
-        impl Handler<u64> for PaperQueryHandler {
-            fn handle(mut self, o: u64) -> HashSet<u64> {
-                self.handle_calls.push(o);
-                match o {
-                    1 => {
-                        let mut results = HashSet::new();
-                        results.insert(2);
-                        results.insert(3);
-                        results
-                    }
-                    2 => {
-                        let mut results = HashSet::new();
-                        results.insert(5);
-                        results
-                    }
-                    _ => HashSet::new()
+                2 => {
+                    let mut results = HashSet::new();
+                    results.insert(5);
+                    results
                 }
-            }
-
-            fn results(self) -> HashSet<(u64, u64)> {
-                self.results()
+                _ => HashSet::new()
             }
         }
 
-        let handler = ReactorHandler::new(PaperQuery{}, PaperQueryHandler::new());
+        fn paper_query(other: &u64) -> bool {
+            *other == 1 || *other == 2
+        }
+        
+        let handler = ReactorHandler::new(handle, paper_query);
 
         let mut reactor = Reactor::new();
         reactor.add_handler(handler);
 
+        reactor.tick();
         // So if we call: 𝗂𝗇𝗌𝖾𝗋𝗍(𝔇0, 𝑜1), then...
-        reactor.add_input(1);
-
-        let reading_handler = &reactor.handlers[0];
-
-
-
-
+        reactor.insert(1);
+        reactor.tick();
+        assert_eq!(reactor.ref_counts.get(&1).is_some(), true);
+        assert_eq!(reactor.ref_counts.get(&1).unwrap(), &1);
+        
+        
+        
     }
 }
